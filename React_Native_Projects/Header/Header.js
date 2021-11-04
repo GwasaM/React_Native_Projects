a19ab232c7deda562040dd6df6e98094d24426ab
Peter Gwasa Machumi <petermachumi262@gmail.com>
2021-11-04 10:51:11 +0300

    my First Git  to github upload

---
+++ React_Native_Projects/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+const Header = (props) =>{
+    return(
+        <View 
+        style = {styles.View}
+        >
+            <Text style = {styles.text}
+            >
+                React Native Application 
+            </Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    view: {
+        width:'100%',
+        height: 50,
+        backgroundColor: '#00f',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text:{
+        fontSize: 25,
+        fontWeight:'bold',
+        color:'#ffffff'
+    }
+})
+
+export default Header;
